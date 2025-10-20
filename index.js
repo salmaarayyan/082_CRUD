127a4ec60687d8d06aba12060b94ca1c1f0677cd
salmaarayyan <salmaarayyan@gmail.com>
2025-10-20 10:49:17 +0700

    memperbaiki code method POST

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/api/users', (req, res) => {
 
 // method POST
 app.post('/api/users', (req, res) => {
-    const { nama, nim, kelas } = req.body;
+    const { nama, nim, kelas } = req.body || {};
     
     if (!nama || !nim || !kelas) {
         return res.status(400).json({ message: 'nama, nim, kelas wajib diisi' });
