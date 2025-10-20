f40a6c161a7db478d153baf3d5843874bab0b5f6
salmaarayyan <salmaarayyan@gmail.com>
2025-10-20 10:04:04 +0700

    Membuat method POST untuk menambah data baru ke database

--- index.js
+++ index.js
@@ -41,4 +41,24 @@ app.get('/api/users', (req, res) => {
     });
 });
 
+// method POST
+app.post('/api/users', (req, res) => {
+    const { nama, nim, kelas } = req.body;
+    
+    if (!nama || !nim || !kelas) {
+        return res.status(400).json({ message: 'nama, nim, kelas wajib diisi' });
+    }
 
+    db.query(
+        'INSERT INTO mahasiswa (nama, nim, kelas) VALUES (?, ?, ?)',
+        [nama, nim, kelas],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Database error' });
+            }
+
+            res.status(201).json({ message: 'User created successfully' });
+        }
+    );
+});
