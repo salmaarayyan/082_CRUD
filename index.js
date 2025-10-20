c80151dde9be05222dc8f4f7bd577e7c688f41e5
salmaarayyan <salmaarayyan@gmail.com>
2025-10-20 09:48:10 +0700

    Inisialisasi server Express dan koneksi database MySQL

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '2315',
+    database: 'mahasiswa',
+    port: 3307
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the MySQL:' + err.stack);
+        return;
+    }
+    console.log('Connection Successfully!');
+});
